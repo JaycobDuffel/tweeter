e603ec60c72948fff60b9c07656a6a23c7508567
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-16 15:32:38 +0000

    refactored counter logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 });
 
 $("#tweet-text").keyup(function() {
-  let count = this.value.length + 1;
-  $(".counter").text(141 - count)
+  let count = this.value.length;
+  $(".counter").text(140 - count)
   if (this.value.length > 140) {
     $(".counter").addClass("red");
   } else {
