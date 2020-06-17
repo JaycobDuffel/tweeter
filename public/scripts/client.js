55a9947c4c4bae86dbb2a5a2af97af412e3d1620
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-17 20:06:07 +0000

    implemented ajax post and get

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,23 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(() => {
-  $(document).ready(function() {
-    $(document).scroll(function () {
-        var scroll = $(this).scrollTop();
-        var topDist = $("#header").position();
-        if (scroll > topDist.top) {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    })
-});
 
-  $("#tweetsArticle").hover(function() {
-    $(this).addClass('shadowed')
-    $("#handle").removeClass('username')
-  }, function(){
-    $(this).removeClass('shadowed')
-    $("#handle").addClass('username')
+  const createTweetElement = (tweetData) => {
+    const $tweet = $(`
+    <article class="tweetArticle">
+      <header>
+        <div id="avatar">
+          <img src="${tweetData.user.avatars}">
+          <span id="name">${tweetData.user.name}</span>
+        </div>
+        <span class="handle username">${tweetData.user.handle}</span>
+      </header>
+      <span id="tweetSpan">${tweetData.content.text}</span>
+      <footer>
+        <span>${new Date(tweetData.created_at).toDateString()}</span>
+        <div class="tweetButtons">
+          <a href="#"><img src="/images/002-flag.png" alt="flag-tweet"></a>
+          <a href="#"><img src="/images/001-retweet.png" alt="retweet"></a>
+          <a href="#"><img src="/images/003-love.png" alt="heart-tweet"></a>
+        </div>
+      </footer>
+    </article>`)
+
+    return $tweet;
+  }
+
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet)
+      $('#tweet-container').prepend($tweet)
+    }
+  }
+
+  $(document).ready(function () {
+    $(document).scroll(function () {
+      var scroll = $(this).scrollTop();
+      var topDist = $("#header").position();
+      if (scroll > topDist.top) {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    })
+  });
+
+  $(function () {
+    const $button = $('#submitTweetBtn')
+    $button.on("click", (event) => {
+      event.preventDefault(event);
+      const data = $("#txtArea").serialize();
+      $.post("/tweets", data)
+        .then(function () {
+          loadTweets()
+        })
+    })
   })
+
+  const loadTweets = () => {
+    $.getJSON('/tweets')
+      .then(function (data) {
+        $('#tweet-container').empty();
+        renderTweets(data);
+      })
+  }
+
+  loadTweets();
 })
